672859511c83c3245d86e47cd0cf5907c303ec22
Misho1019 <mikailtorres99@gmail.com>
2025-04-17 02:24:42 +0300

    Parse form data from react forms

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import routes from "./routes.js";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => {
-    res.send('It works!')
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(port, () => console.log(`Server is listening on http://localhost:${port}...`))
