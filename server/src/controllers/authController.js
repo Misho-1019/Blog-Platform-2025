2415b9a0f70435577a7ea82049b305a590357c7b
Misho1019 <mikailtorres99@gmail.com>
2025-04-17 04:19:18 +0300

    Add auth service with register

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import authService from "../services/auth-service.js";
 
 const authController = Router();
 
 authController.post('/register', async (req, res) => {
     const authData = req.body;
 
-    console.log(authData);
+    await authService.register(authData)
     
     res.end();
 })
