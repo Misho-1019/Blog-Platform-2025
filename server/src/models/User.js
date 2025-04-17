bcd6add6a9770aa0e327ab5cd0b3e611e82ba047
Misho1019 <mikailtorres99@gmail.com>
2025-04-17 19:59:28 +0300

    Modify User model validation

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,14 +4,17 @@ import bcrypt from "bcrypt";
 const userSchema = new Schema({
     email: {
         type: String,
+        required: true,
         unique: true,
         match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
+        lowercase: true,
         minLength: 10,
     },
     password: {
         type: String,
         match: /^\w+$/,
         minLength: 6,
+        trim: true,
     },
 })
 
