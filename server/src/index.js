ef7b4782358fd815952ae469f7a697a3a32d3478
Misho1019 <mikailtorres99@gmail.com>
2025-04-17 04:08:38 +0300

    Set up mongoose successfully

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,19 @@
 import express from "express";
 import routes from "./routes.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+try {
+    const uri = 'mongodb+srv://mikail:Mvt12345@cluster0.0jspcgh.mongodb.net/Blog-Platform-2025?retryWrites=true&w=majority&appName=Cluster0'
+    await mongoose.connect(uri)
+
+    console.log('DB Connected Successfully!');
+} catch (err) {
+    console.log('Cannot connect to DB!');
+    console.error(err.message);
+}
+
 const port = process.env.PORT || 3030;
 
 app.use(express.json())
