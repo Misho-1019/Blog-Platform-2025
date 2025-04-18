f42f16982ea81e77972ecc561f9904319cd96c7b
Misho1019 <mikailtorres99@gmail.com>
2025-04-18 06:51:28 +0300

    Use action state for login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,6 +1,18 @@
+import { useActionState } from 'react';
 import styles from './Login.module.css';
 
-export default function Login() {
+export default function Login({
+    onLogin
+}) {
+    const loginHandler = (previousState, formData) => {
+        const values = Object.fromEntries(formData)
+
+        onLogin(values.email)
+
+        return values;
+    }
+
+    const [values, loginAction, isPending] = useActionState(loginHandler, { email: '', password: '' })
     return (
         <div className={styles.container}>
             <div className={styles.formBox}>
