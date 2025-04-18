c06b7bfc953d55c2cbd3d54bb35d9810d76759b0
Misho1019 <mikailtorres99@gmail.com>
2025-04-19 01:20:32 +0300

    Modify User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,6 +2,10 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
