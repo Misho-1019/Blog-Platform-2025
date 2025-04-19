d3eb3b4a1cbfacbd282ad7610f567eeaf60d4fe7
Misho1019 <mikailtorres99@gmail.com>
2025-04-19 05:56:22 +0300

    Install cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import routes from "./routes.js";
 import { authMiddleware } from "./middlewares/authMiddleware.js";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 try {
     const uri = 'mongodb+srv://mikail:Mvt12345@cluster0.0jspcgh.mongodb.net/Blog-Platform-2025?retryWrites=true&w=majority&appName=Cluster0'
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,10 +2,6 @@ import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
     email: {
         type: String,
         required: true,
